5202c451e60e5c1c20dd2c7f316a46124ab1d0a4
Kirill Yudin <det0x2h@gmail.com>
2018-05-22 14:45:43 +0700

    NewBase

--- school_unity/Login.xaml.cs
+++ school_unity/Login.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using school_unity.DataSet1TableAdapters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,52 @@ namespace school_unity
         public Login()
         {
             InitializeComponent();
+        }
+
+        DataSet1 ds = new DataSet1();
+        UserTableAdapter uta = new UserTableAdapter();
+
+
+     
+
+        private void EnterButton_Click(object sender, RoutedEventArgs e)
+        {
+            string Login = LoginTB.Text;
+           // school_unity.PubLogin = Login; //Запоминаем логин пользователя, для дальнейшего взаимодействия
+            string Password = PasswordTB.Password;
+
+
+            var logining = uta.Login(ds.User, Login, Password); // Проверяем наличие в базе данных
+            if (logining == 1)
+            {
+                string UserRole = (string)ds.User[0]["RoleID"]; // Настраиваем переходы в зависимости от роли
+
+                if (UserRole == "1")
+                {
+                    NavigationService.Navigate(new UserMenu.admin());
+                }
+
+                if (UserRole == "2")
+                {
+                    NavigationService.Navigate(new UserMenu.schoolkid());
+                }
+                
+                if (UserRole == "3")
+                {
+                    NavigationService.Navigate(new UserMenu.teacher());
+                }
+
+                if (UserRole == "4")
+                {
+                    NavigationService.Navigate(new UserMenu.parent());
+                }
+
+            }
+            else //Обрабатываем исключения
+            {
+                MessageBox.Show("Неправильный логин или пароль!");
+            }
+
         }
 
         private void Rectangle_MouseUp(object sender, MouseButtonEventArgs e)
